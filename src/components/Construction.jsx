06f369dae520cf0c1c4167ff4406ede411c0fb01
Yinghao Alan Wang <yinghao.a.wang@gmail.com>
2024-08-06 16:01:01 -0700

    add social links

--- src/components/Construction.jsx
+++ src/components/Construction.jsx
@@ -5,8 +5,29 @@ export default function Construction() {
         <img width='200px' src='barrier.svg' />
         <div className='headline'>Under Construction</div>
         <div className='description'>
-          Sorry about the mess, the site is current being worked on, but feel
-          free to contact me!
+          <span>
+            Sorry about the mess, the site is current being worked on, but feel
+            free to contact me!
+          </span>
+          <div className='social-icons' style={{ marginTop: '20px' }}>
+            <a className='custom-icon' href='mailto:yinghao.a.wang@gmail.com'>
+              <img width='32px' src='mail.svg' />
+            </a>
+            <a
+              className='custom-icon linkedin'
+              href='https://linkedin.com/in/yinghao-alan-wang'
+              target='_blank'
+            >
+              <img width='24px' src='linkedin.svg' />
+            </a>
+            <a
+              className='custom-icon'
+              href='https://github.com/yinghaoawang'
+              target='_blank'
+            >
+              <img style={{ marginTop: '9px' }} width='42px' src='github.svg' />
+            </a>
+          </div>
         </div>
       </div>
     </div>
