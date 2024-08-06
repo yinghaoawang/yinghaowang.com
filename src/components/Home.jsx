5a2f03f8c2e57f73192788c8e043112c21511325
Yinghao Alan Wang <yinghao.a.wang@gmail.com>
2024-08-06 11:28:41 -0700

    fix responsiveness on headline & gif showing

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,9 @@ export default function Home() {
   return (
     <div className='home'>
       <div className='message'>
-        <div className='headline'>ALAN WANG</div>
+        <div className='headline'>
+          <span>ALAN</span> <span>WANG</span>
+          </div>
         <div className='description'>
           TECH SUPPORT & FULL STACK DEVELOPER
         </div>
