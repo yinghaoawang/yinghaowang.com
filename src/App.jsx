74b6fa3cdbfd51c9ab330054be8ae0d9ebb1fd27
Yinghao Alan Wang <yinghao.a.wang@gmail.com>
2024-08-06 12:12:22 -0700

    add under construction section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Projects from './components/Projects';
+import Construction from './components/Construction';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <div className='section-wrapper'>
         <Home />
         <Projects />
+        <Construction />
       </div>
     </>
   );
---
+++ src/components/Construction.jsx
@@ -0,0 +1,14 @@
+export default function Construction() {
+  return (
+    <div className='construction'>
+      <div className='inner-wrapper'>
+        <img width='200px' src='barrier.svg' />
+        <div className='headline'>Under Construction</div>
+        <div className='description'>
+          Sorry about the mess, the site is current being worked on, but feel
+          free to contact me!
+        </div>
+      </div>
+    </div>
+  );
+}
